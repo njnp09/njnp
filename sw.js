105f9ef44d3857cef7df3079f98106f538aba363
njnp09 <njnp09@gmail.com>
2026-07-17 19:57:45 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME="clickset-official-1.0.1";
-const APP_SHELL=["./","./index.html","./clickset-1.0.1.css","./clickset-1.0.1.js","./manifest.webmanifest","./cover.png","./click.wav","./logo-4l.jpg","./logo-h80.jpeg","./logo-tocipam.jpg","./logo-pgs.png","./logo-clickset.png"];
+const CACHE_NAME="clickset-official-1.0.2";
+const APP_SHELL=["./","./index.html","./clickset-1.0.2.css","./clickset-1.0.2.js","./manifest.webmanifest","./cover.png","./click.wav","./logo-4l.jpg","./logo-h80.jpeg","./logo-tocipam.jpg","./logo-pgs.png","./logo-clickset.png"];
 self.addEventListener("install",event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)))});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
 self.addEventListener("fetch",event=>{
