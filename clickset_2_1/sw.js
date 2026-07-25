2cf92232376a466d4807fe1ceb64f9806a54d536
njnp09 <njnp09@gmail.com>
2026-07-25 19:06:26 +0200

    Add files via upload

---
+++ clickset_2_1/sw.js
@@ -0,0 +1,5 @@
+const CACHE='clickset-2.1-static';
+const FILES=['./','./index.html','./clickset-2.0.css','./clickset-2.0.js','./manifest.webmanifest','./cover.png','./logo-clickset.png','./logo-4l.jpg','./logo-h80.jpeg','./logo-pgs.png','./logo-tocipam.jpg','./claves-normal.wav','./claves-accent.wav','./cowbell-normal.wav','./cowbell-accent.wav','./rimshot-normal.wav','./rimshot-accent.wav','./clap-normal.wav','./clap-accent.wav','./click.wav'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html'))))});
